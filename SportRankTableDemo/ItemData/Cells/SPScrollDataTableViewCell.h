0c9429476d2052460fc465211cb62b75b5ecd206
toolazytoname <shuitaiyang747@qq.com>
2014-12-05 18:19:01 +0800

    增加文件

---
+++ SportRankTableDemo/ItemData/Cells/SPScrollDataTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  SPScrollDataTableViewCell.h
+//  SportRankTableDemo
+//
+//  Created by SINA on 14/12/4.
+//  Copyright (c) 2014年 Sport. All rights reserved.
+//
+
+#import "SPBaseDataTableviewCell.h"
+
+@interface SPScrollDataTableViewCell : SPBaseDataTableviewCell
+
+@end
---
+++ SportRankTableDemo/ItemData/Cells/SPScrollDataTableViewCell.m
@@ -0,0 +1,25 @@
+//
+//  SPScrollDataTableViewCell.m
+//  SportRankTableDemo
+//
+//  Created by SINA on 14/12/4.
+//  Copyright (c) 2014年 Sport. All rights reserved.
+//
+
+#import "SPScrollDataTableViewCell.h"
+@interface SPScrollDataTableViewCell()
+@property (strong, nonatomic) IBOutlet UIScrollView *cellScrollView;
+
+@end
+
+@implementation SPScrollDataTableViewCell
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
